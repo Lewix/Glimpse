546d93fa05a34b0188a045ee471dcd357aaa2303
Colin Rothwell <colin.rothwell@red-gate.com>
2012-08-14 12:03:35 +0100

    Insert generated script tags from Configuration before body tag

--- source/GlimpsePython/glimpse/middleware.py
+++ source/GlimpsePython/glimpse/middleware.py
@@ -1,18 +1,24 @@
+from configuration import Configuration
+
 class Middleware(object):
     def __init__(self, application):
         self._application = application
+        self._config = Configuration()
 
     def __call__(self, environ, start_response):
+        def filter_data(data):
+            return (data.replace('</body>', self._config.generate_script_tags()))
+
         def start_glimpse_response(status, response_headers, exc_info=None):
             write = start_response(status, response_headers, exc_info)
 
             def glimpse_write(data):
-                write(data + 'script tag')
+                write(filter_data(data))
 
             return glimpse_write
         
         data = self._application(environ, start_glimpse_response)
-        return [item + 'script tag from later iteration' for item in data]
+        return (filter_data(item) for item in data)
 
 def wrap_application(wsgi_application):
     return Middleware(wsgi_application)
